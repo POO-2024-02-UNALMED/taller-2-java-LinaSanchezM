38dad45c4b12c4165907e4a3fae7af06821cd568
LinaSanchezM <lsanchezmo@unal.edu.co>
2024-11-10 16:25:18 -0500

    prueba

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,7 +1,7 @@
 package test;
 
 public class Asiento {
-    private String[] colors = {"rojo", "verde", "azul", "amarillo", "negro", "blanco"};
+    static String[] colors = {"rojo", "verde", "azul", "amarillo", "negro", "blanco"};
     String color;
     int precio;
     int registro;
