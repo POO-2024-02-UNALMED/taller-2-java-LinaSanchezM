933912de390b2b1d4beb388277e7568ba7db29c7
LinaSanchezM <lsanchezmo@unal.edu.co>
2024-11-10 17:03:44 -0500

    prueb

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -4,6 +4,7 @@ public class Motor {
     String tipo;
     int numeroCilindros;
     int registro;
+    static String[] tipopos = {"electrico", "gasolina"};
 
     void cambiarRegistro(int registro){
         this.registro=registro;
@@ -11,5 +12,9 @@ public class Motor {
 
 
     void asignarTipo(String tipo){
+        for (String i : tipopos) {
+    if(i.equals(tipo)){
+        this.tipo= tipo;
+        brak;
     }
 }
