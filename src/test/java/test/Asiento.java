94f9dc064a3f9ef4cd625091add956e0b6c763a8
Lina Marcela Sánchez Morales <lsanchezmo@unal.edu.co>
2024-11-10 20:15:46 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,18 +1,18 @@
 package test;
 
+
 public class Asiento {
-    static String[] colors = {"rojo", "verde", "azul", "amarillo", "negro", "blanco"};
+    static String[] coloresPermitidos = {"rojo", "verde", "azul", "amarillo", "negro", "blanco"};
     String color;
     int precio;
     int registro;
 
     void cambiarColor(String color){
-        for (String i : colors) {
+        for (String i : coloresPermitidos) {
             if(i.equals(color)){
                 this.color= color;
                 break;
             }
-            this.color = color;
         }    
     }
 }
