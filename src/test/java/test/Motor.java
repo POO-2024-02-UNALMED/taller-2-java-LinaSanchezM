d66c8d52ef30b0668fa3e1c4a5e0ae8fa1145832
Lina Marcela Sánchez Morales <lsanchezmo@unal.edu.co>
2024-11-10 20:16:22 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -4,17 +4,16 @@ public class Motor {
     String tipo;
     int numeroCilindros;
     int registro;
-    static String[] tipopos = {"electrico", "gasolina"};
 
     void cambiarRegistro(int registro){
         this.registro=registro;
     }
 
-
     void asignarTipo(String tipo){
-        for (String i : tipopos) {
-    if(i.equals(tipo)){
-        this.tipo= tipo;
-        brak;
+        if (tipo == "electrico" || tipo == "gasolina" ){
+            this.tipo= tipo;
+        }
     }
+
+
 }
